bab2dd0954cc8670d1fbc6f2c25fbcee884fdf4d
Yosef Bayoude <yosef.bayoude@gmail.com>
2019-11-19 22:27:18 +0100

    ajout file de message

--- projet.c
+++ projet.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/msg.h>
 
+
+#define cle 314
 
 typedef struct type_processus
 {
+    long type;
     int priorite;
     int date_soumission;
     int temps_exec;
@@ -33,6 +38,11 @@ int main()
     printf("Lancement du programme\n");
     srand(time(0));
 
+    int msgid;
+    if ((msgid = msgget(cle, 0750 | IPC_CREAT | IPC_EXCL)) == -1)
+    {
+        perror("Erreur creation file de message\n");
+    }
 
 
     printf("Creation du fils\n");
@@ -42,22 +52,51 @@ int main()
         case -1:
             perror("Erreur fork");
             break;
+
+
+        /****************************************************/
         case 0:
+            {
             //FILS
             //Genere des processus
+
+
+            
             for (int i = 0; i < randomNumber(10, 0); i++)
             {
                 processus prc = genererProcessus();
+                prc.type = getpid();
+                msgsnd(msgid, &prc, sizeof(processus) - sizeof(long), 0);
                 printf("Processus prio : %d\n", prc.priorite);
             }
 
             exit(0);
+            }
             break;
+
+
+        /****************************************************/
         default:
             //PERE
             //Gere l'algorithme d'ordonnancement
+            {
+            int status;
+            wait(&status);
+            processus liste_prc[20];
+
+            struct msqid_ds buf;
+            int nb_messages = msgctl(msgid, IPC_STAT, &buf);
+
+            msgrcv(msgid, &liste_prc[0], sizeof(processus) - sizeof(long), pid_fils, 0);
+
+            printf("Message recu = %d\n", liste_prc[0].priorite);
+
+            printf("Il reste %ld messages dans la file\n", buf.msg_qnum);
+            }
             break;
     }
+
+    msgctl(msgid, IPC_RMID, NULL);
 
     printf("Fin du programme\n");
     return 0;
