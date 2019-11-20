1c17445fa7d009f05ad7fa8320f776f10dfcdf60
Yosef Bayoude <yosef.bayoude@gmail.com>
2019-11-20 17:48:25 +0100

    ajout memoire partagé et P() & V()

--- projet.c
+++ projet.c
@@ -5,10 +5,22 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/shm.h>
 #include <sys/msg.h>
+#include <signal.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
 
+#define CLE 314
 
-#define cle 314
+int msgid;
+int semid;
+int shmid;
+
+int* temps;
+
+struct sembuf sem_oper_P ;  /* Operation P */
+struct sembuf sem_oper_V ;  /* Operation V */
 
 typedef struct type_processus
 {
@@ -16,33 +28,92 @@ typedef struct type_processus
     int priorite;
     int date_soumission;
     int temps_exec;
-} processus ;
+} processus;
 
+void P() {
+    sem_oper_P.sem_num = 0;
+    sem_oper_P.sem_op  = -1 ;
+    sem_oper_P.sem_flg = 0 ;
+    semop(semid, &sem_oper_P, 1);
+}
 
-int randomNumber(int max, int min){
+void V() {
+    sem_oper_V.sem_num = 0;
+    sem_oper_V.sem_op  = 1 ;
+    sem_oper_V.sem_flg  = 0 ;
+    semop(semid, &sem_oper_V, 1);
+}
+
+int randomNumber(int min, int max){
     if (min > max) perror("Erreur randomNumber\n");
     return (rand() %(max - min + 1)) + min;
+}
+
+int initsem(key_t semkey) 
+{
+    int status;
+	int semid_init;
+   	union semun {
+		int val;
+		struct semid_ds *stat;
+		short * array;
+	} ctl_arg;
+    short array[1] = {1};
+    ctl_arg.array = array;
+
+    if ((semid_init = semget(semkey, 1, 0666|IPC_CREAT)) > 0) {
+        status = semctl(semid_init, 0, SETALL, ctl_arg);
+    }
+    if (semid_init == -1 || status == -1) { 
+        perror("Erreur initsem\n");
+        return (-1);
+    }else{
+        return semid_init;
+    }
 }
 
 
 processus genererProcessus(){
     processus prc;
-    prc.priorite = (rand() %(10 - 0 + 1)) + 0;
-    prc.temps_exec = (rand() %(20 - 0 + 1)) + 0;
+    prc.priorite = randomNumber(0, 10);
+    prc.temps_exec = randomNumber(0, 20);
     return prc;
 }
 
+void supprimerMessages(){
+    msgctl(msgid, IPC_RMID, NULL);
+}
+
+
+void traitandSIGINT(int num){
+    if (num == SIGINT){
+        supprimerMessages();
+	    shmctl(shmid, IPC_RMID, NULL);
+        semctl(semid, 0, IPC_RMID, NULL);
+        exit(1);
+    } else {
+        perror("Erreur traitant, files non supprimé\n");
+    }
+}
 
 int main()
 {
     printf("Lancement du programme\n");
     srand(time(0));
 
-    int msgid;
-    if ((msgid = msgget(cle, 0750 | IPC_CREAT | IPC_EXCL)) == -1)
+
+    signal(SIGINT, traitandSIGINT);
+    msgid;
+    if ((msgid = msgget(CLE, 0750 | IPC_CREAT | IPC_EXCL)) == -1)
     {
         perror("Erreur creation file de message\n");
     }
+
+    semid = initsem(CLE);
+
+    shmid = shmget(CLE, sizeof(int), 0666|IPC_CREAT);
+	temps = (int *)shmat(shmid, NULL, 0);
+	*temps = 0; //initialisation a 0 du compteur
 
 
     printf("Creation du fils\n");
@@ -55,23 +126,28 @@ int main()
 
 
         /****************************************************/
-        
         case 0:
             {
-            //FILS
-            //Genere des processus
+                //FILS
+                //Genere des processus
 
 
-            
-            for (int i = 0; i < randomNumber(10, 0); i++)
-            {
-                processus prc = genererProcessus();
-                prc.type = getpid();
-                msgsnd(msgid, &prc, sizeof(processus) - sizeof(long), 0);
-                printf("Processus prio : %d\n", prc.priorite);
-            }
+                
+                while(1) {
+                    P();
+                    for (int i = 0; i < randomNumber(0, 10); i++)
+                    {
+                        processus prc = genererProcessus();
+                        prc.type = getpid();
+                        prc.date_soumission = *temps;
+                        msgsnd(msgid, &prc, sizeof(processus) - sizeof(long), 0);
+                        printf("Processus arrivé avec prio : %d\n", prc.priorite);
+                    }
+                    sleep(2);
+                    V();
+                }
 
-            exit(0);
+                exit(0);
             }
             break;
 
@@ -81,18 +157,29 @@ int main()
             //PERE
             //Gere l'algorithme d'ordonnancement
             {
-            int status;
-            wait(&status);
-            processus liste_prc[20];
+                //int status;
+                //wait(&status);
+                
+                processus liste_prc[20];
 
-            struct msqid_ds buf;
-            int nb_messages = msgctl(msgid, IPC_STAT, &buf);
+                while(1) {
+                    P();
+                    struct msqid_ds buf;
+                    int nb_messages = msgctl(msgid, IPC_STAT, &buf);
+                    while (buf.msg_qnum > 0) //Tant qu'il y a des messages
+                    {
+                        msgrcv(msgid, &liste_prc[0], sizeof(processus) - sizeof(long), pid_fils, 0);
+                        nb_messages = msgctl(msgid, IPC_STAT, &buf);
+                        printf("Message recu : prio = %d, temps = %d\n", liste_prc[0].priorite, liste_prc[0].date_soumission);
+                    }
+                    sleep(5);
+                    (*temps)++;
+                    printf("Temps courant : %d\n", *temps);
+                    V();
+                }
 
-            msgrcv(msgid, &liste_prc[0], sizeof(processus) - sizeof(long), pid_fils, 0);
+                //printf("Il reste %ld messages dans la file\n", buf.msg_qnum);
 
-            printf("Message recu = %d\n", liste_prc[0].priorite);
-
-            printf("Il reste %ld messages dans la file\n", buf.msg_qnum);
             }
             break;
     }
